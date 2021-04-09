cd0eab7078ca0c6a8ad22278eadfc7576324c475
Roy Mosby <ego.madking@gmail.com>
2021-04-09 10:49:52 -0400

    added filter method to get to notes in join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
 
   has_many :user_installed_gems
   has_many :installed_gems, through: :user_installed_gems
+
+  def gem_notes(installed_gem)
+    ig = UserInstalledGem.where("user_id == ? AND installed_gem_id == ?", self.id, installed_gem.id).take
+    ig.user_notes
+  end
+  
 end
